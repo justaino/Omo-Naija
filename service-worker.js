95ba5e91a062bb5efa4db5c94e5600e8ce65a4e3
Justice Iria <jujust97@gmail.com>
2026-06-27 22:39:56 +0100

    Set absolute start_url and scope in manifest

--- service-worker.js
+++ service-worker.js
@@ -6,7 +6,7 @@
 //
 // IMPORTANT: bump CACHE on every deploy that changes files, so installed
 // devices pick up the new version (the old cache is cleaned on activate).
-const CACHE = 'omo-naija-v17';
+const CACHE = 'omo-naija-v18';
 
 const PRECACHE = [
   './',
